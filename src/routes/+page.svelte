80c81b3100ce51c7bb7b104282d62c5401ad0ec0
Saad El Abboubi <elabboubisaad3@gmail.com>
2023-04-08 23:59:40 +0000

    taha lkobra ki dour

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,15 @@
+<script>
+    import Component from "./component.svelte";
+</script>
+
+
 <body class="body h-screen w-screen flex flex-row justify-center ">
     <div class="gameborder grid grid-rows-6 grid-cols-6 bg-emerald-200  h-2/3 w-1/2 self-center ">
+        <div class="flex flex-row">
+            <Component />
+
+        </div>
+
 
     </div>
     
