b6db4d105a47b246b16c1fe9461ec45162cb7852
Saad El Abboubi <elabboubisaad3@gmail.com>
2023-04-09 18:37:47 +0000

    taha lkobra navigue bien

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,75 @@
-<script>
+<script lang="ts">
     import Component from "./component.svelte";
+    let x_value : number = 0
+    let x_top : number = 0 
+    let x_direction : string = ""
+    let angle : number = 0 ;
+    let rotation : boolean = false;
+
+    function onKeyDown(e) {
+
+    switch(e.keyCode) {
+      case 39:
+      x_direction="right"
+      rotation = false;
+      angle = 0;
+      break;
+
+     case 40:
+      x_direction="down"
+      rotation = false;
+      angle = 90;
+      break;
+
+      case 38:
+        x_direction="up"
+        rotation = true;
+        angle = 90;
+        break;
+
+      case 37: 
+      x_direction="left"
+      rotation = false;
+      angle = 180;
+      break;
+
+      default:
+        break;
+    }
+  }
+
+function moovesnake(){
+  if(x_direction=="right"){
+    x_value+=16
+ 
+  }
+  else if(x_direction=="left"){
+    x_value-=16
+  }
+  
+  else if(x_direction=="down"){
+    x_top+=16
+  }
+  else if(x_direction=="up"){
+    x_top-=16
+  }
+  
+  
+  
+  
+  
+  
+
+}
+setInterval(moovesnake,50)
+$:moovesnake(x_direction)
 </script>
 
 
 <body class="body h-screen w-screen flex flex-row justify-center ">
     <div class="gameborder grid grid-rows-6 grid-cols-6 bg-emerald-200  h-2/3 w-1/2 self-center ">
-        <div class="flex flex-row">
-            <Component />
+        <div class="flex flex-row relative" style="left: {x_value}px; top: {x_top}px" on:keydown={onKeyDown}  >
+            <Component angle={angle} rotation={rotation}/>
 
         </div>
 
@@ -14,3 +77,4 @@
     </div>
     
 </body>
+<svelte:window on:keydown={onKeyDown}  />
--- src/routes/component.svelte
+++ src/routes/component.svelte
@@ -1,34 +1,12 @@
 <script lang="ts">
   import logo from '$lib/assets/taha_lkobra.jpg';
 
-  let angle = 0;
-  let negative : boolean = false;
+  export let angle : number ;
+  export let rotation : boolean ;
 
-  function onKeyDown(e) {
-    switch(e.keyCode) {
-      case 38:
-        negative = true;
-        angle = 90;
-        console.log(negative)
-        break;
-      case 39:
-        negative = false;
-        angle = 0;
-        break;
-      case 40:
-        negative = false;
-        angle = 90;
-        break;
-      default:
-        break;
-    }
-  }
 
 
 </script>
 
-<div class="snake-body w-20 h-12 bg-green-600 border border-black"></div>
-<div class="snake-body w-20 h-12 bg-green-600 border border-black"></div>
-<img alt="tahalkobra" src={logo} class={`w-20 h-12 ${negative ? '-rotate-90' : `rotate-${angle}`}`} />
+<img alt="tahalkobra" src={logo} class={`w-12 h-12 ${rotation ? '-rotate-90' :`rotate-${angle}`}`} />
 
-<svelte:window on:keydown={onKeyDown}  />
